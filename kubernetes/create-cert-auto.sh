3749e8c41c3faedbb33c8fc3e91e1f4180156dfa
YutaroKotani <kotani@rhems-japan.co.jp>
2024-09-25 11:05:08 +0900

    save

--- kubernetes/create-cert-auto.sh
+++ kubernetes/create-cert-auto.sh
@@ -29,7 +29,49 @@ if [ -f ./getssl.log ]; then
   rm ./getssl.log
 fi
 
-./getssl -f "${_domain}" 2>&1
+./getssl -f "${_domain}" 2>&1 | tee -a getssl.log
+
+if cat getssl.log | grep -qE 'Verification completed, obtaining certificate'; then
+  echo "Certificate created successfully"
+  echo "certificate upload to cert manager"
+  cd /root/.getssl/"${_domain}"
+  _log=""
+  if [ "${CLOUD}" == "aws" ]; then
+    echo "aws"
+    aws acm import-certificate --certificate fileb://"${_domain}".crt --certificate-chain fileb://chain.crt --private-key fileb://"${_domain}".key | tee -a getssl.log
+    _log=$(cat getssl.log | jq -r '.CertificateArn' | jq -sRr @uri)
+  elif [ "${CLOUD}" == "tencent" ]; then
+    echo "tencent"
+    _alias="cert_$(date '+%Y%m%d%H%M%S')"
+    _private_key=$(cat "${_domain}".key)
+    _public_key=$(cat "${_domain}".crt)
+    tccli ssl UploadCertificate --CertificatePublicKey "${_public_key}" --CertificatePrivateKey "${_private_key}" --CertificateType SVR --output json --Alias "${_alias}" | tee -a getssl.log
+    _log=$(cat getssl.log | jq -r '.CertificateId' | jq -sRr @uri)
+
+    _cert_secret_name=$(kubectl get secrets -n "${POD_NAMESPACE}" -o json | jq -r '.items[].metadata.name' | grep -e '^certificate-[a-z,0-9]*')
+    kubectl patch secret -n "${POD_NAMESPACE}" "${_cert_secret_name}" -p '{"data":{"qcloud_cert_id":"'"${_log}"'"}}'
+
+    _log="update qcloud_cert_id in secret. Namespace: ${POD_NAMESPACE}, secret name: ${_cert_secret_name}"
+  fi
+  curl -X POST -H "Content-Type: application/json" \
+        https://badges.rhems-japan.com/api-update-badge \
+        -d '{
+              "api_token": "'"${API_TOKEN}"'",
+              "organization": "'"${ORGANIZATION}"'",
+              "repo": "'"${REPO}"'",
+              "app": "'"${APP}"'",
+              "branch": "'"${BRANCH}"'",
+              "status": true,
+              "update": "'$(date +%Y-%m-%d-%H-%M-%S)'",
+              "cronjob": "'"${CRON}"'",
+              "grace_time": '"${GRACE_TIME}"',
+              "slack_failed": "'"${SLACK_FAILED}"'",
+              "slack_success": "'"${SLACK_SUCCESS}"'",
+              "msg": "certificate created successfully.",
+              "log": "'"${_log}"'"
+            }'
+  exit 0
+fi
 
 _file_name=""
 _file_content=""
@@ -140,7 +182,7 @@ if cat getssl.log | grep -qE 'Certificate saved in' ; then
             "msg": "certificate created successfully.",
             "log": "'"${_log}"'"
           }'
-    exit 0
+  exit 0
 fi
 
 #if cat getssl.log | grep -qE 'for some reason could not reach' ; then
@@ -203,5 +245,5 @@ fi
 #            "msg": "certificate created successfully.",
 #            "log": "'"${_log}"'"
 #          }'
-#    exit 0
+#  exit 0
 #fi
--- kubernetes/init.sh
+++ kubernetes/init.sh
@@ -10,3 +10,21 @@ echo "CA='https://acme-staging-v02.api.letsencrypt.org'" >> /root/.getssl/"${_do
 # echo "CA='https://acme-v02.api.letsencrypt.org'" >> /root/.getssl/"${_domain}"/getssl.cfg
 echo "ACL=('/var/www/html/.well-known/acme-challenge')" >> /root/.getssl/"${_domain}"/getssl.cfg
 echo "USE_SINGLE_ACL='true'" >> /root/.getssl/"${_domain}"/getssl.cfg
+
+_configmap_account_key=$(kubectl get cm account-key -o json | jq -r '.data."account.key"');
+
+if [[ "${_configmap_account_key}" == "__CONTENT__" ]] || [[ -z "${_configmap_account_key}" ]]; then
+  echo "account.key is empty"
+  cp account-key-base.yml account-key.yml
+  ./getssl "${_domain}"
+  while read -r line; do
+    printf "    %s\n" "${line}" >> account-key.yml
+  done < /root/.getssl/account.key
+  kubectl apply -f account-key.yml -n "${POD_NAMESPACE}"
+else
+  echo "account.key is not empty"
+  echo "${_configmap_account_key}" > /root/.getssl/account.key
+fi
+
+
+
