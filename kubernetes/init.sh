cef3c4b3288a6cef1ef711b0313d6675666647e0
YutaroKotani <kotani@rhems-japan.co.jp>
2024-10-03 16:32:19 +0900

    save

--- kubernetes/init.sh
+++ kubernetes/init.sh
@@ -7,8 +7,8 @@ mv /tmp/getssl/getssl /root/getssl
 mv /tmp/create-cert /root/create-cert
 ./getssl -c "${_domain}"
 sed -i 's/SANS=/#SANS=/g' /root/.getssl/"${_domain}"/getssl.cfg
-echo "CA='https://acme-staging-v02.api.letsencrypt.org'" >> /root/.getssl/"${_domain}"/getssl.cfg
-#echo "CA='https://acme-v02.api.letsencrypt.org'" >> /root/.getssl/"${_domain}"/getssl.cfg
+#echo "CA='https://acme-staging-v02.api.letsencrypt.org'" >> /root/.getssl/"${_domain}"/getssl.cfg
+echo "CA='https://acme-v02.api.letsencrypt.org'" >> /root/.getssl/"${_domain}"/getssl.cfg
 echo "ACL=('/var/www/html/.well-known/acme-challenge')" >> /root/.getssl/"${_domain}"/getssl.cfg
 echo "USE_SINGLE_ACL='true'" >> /root/.getssl/"${_domain}"/getssl.cfg
 
