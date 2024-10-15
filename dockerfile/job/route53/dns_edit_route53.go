ad5ef9277ee5a37962a5adc18fe0515e4e85f0ba
YutaroKotani <kotani@rhems-japan.co.jp>
2024-10-15 18:12:53 +0900

    save

---
+++ dockerfile/job/route53/dns_edit_route53.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/route53"
+	"github.com/aws/aws-sdk-go-v2/service/route53/types"
+	"os"
+	"path/filepath"
+	"regexp"
+	"sort"
+)
+
+var basename string
+var fqdn string
+var challenge string
+
+func main() {
+	basename = filepath.Base(os.Args[0])
+	fqdn = os.Args[1]
+	challenge = os.Args[2]
+
+	switch basename {
+	case "dns_add_route53":
+		dns_add_route53(fqdn, challenge)
+		break
+	case "dns_remove_route53":
+		dns_remove_route53(fqdn, challenge)
+		break
+	default:
+		fmt.Println("Unknown basename")
+		os.Exit(1)
+	}
+}
+
+func dns_add_route53(fqdn string, challenge string) {
+	fmt.Println("dns_add_route53")
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		fmt.Println("unable to load SDK config, ", err)
+		os.Exit(1)
+	}
+	route53Client := route53.NewFromConfig(cfg)
+	zones, err := route53Client.ListHostedZones(context.TODO(), &route53.ListHostedZonesInput{})
+	if err != nil {
+		fmt.Println("unable to list hosted zones, ", err)
+		os.Exit(1)
+	}
+
+	sort.Slice(zones.HostedZones, func(i, j int) bool {
+		return len(*zones.HostedZones[i].Name) > len(*zones.HostedZones[j].Name)
+	})
+
+	var matchedZoneId string
+	for _, zone := range zones.HostedZones {
+		name := *zone.Name
+		match, _ := regexp.MatchString(name[0:len(name)-1], fqdn)
+
+		if match {
+			fmt.Println("Matched")
+			fmt.Println("zone id: ", filepath.Base(*zone.Id))
+			fmt.Println("zone name: ", name)
+			matchedZoneId = filepath.Base(*zone.Id)
+		}
+	}
+
+	if matchedZoneId == "" {
+		fmt.Println("No matched zone")
+		os.Exit(1)
+	}
+
+	challengeFqdn := fmt.Sprintf("\"_acme-challenge.%s\"", fqdn)
+	comment := "getssl/Letsencrypt verification"
+	var ttl int64 = 300
+	resourceRecord := types.ResourceRecord{
+		Value: &challengeFqdn,
+	}
+	resourceRecordSet := types.ResourceRecordSet{
+		Name: &challengeFqdn,
+		Type: types.RRTypeTxt,
+		TTL:  &ttl,
+		ResourceRecords: []types.ResourceRecord{
+			resourceRecord,
+		},
+	}
+	change := types.Change{
+		Action:            types.ChangeActionUpsert,
+		ResourceRecordSet: &resourceRecordSet,
+	}
+	changeBatch := types.ChangeBatch{
+		Comment: &comment,
+		Changes: []types.Change{
+			change,
+		},
+	}
+
+	createRecordInput := &route53.ChangeResourceRecordSetsInput{
+		HostedZoneId: &matchedZoneId,
+		ChangeBatch:  &changeBatch,
+	}
+
+	_, err = route53Client.ChangeResourceRecordSets(context.TODO(), createRecordInput)
+	if err != nil {
+		fmt.Println("unable to create record, ", err)
+		os.Exit(1)
+	}
+}
+
+func dns_remove_route53(fqdn string, challenge string) {
+	fmt.Print("dns_remove_route53")
+}
